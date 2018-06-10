2ca6ea81bdbf98755423438e47781686779fba9c
Yago López <yago.lopez@gmail.com>
2018-06-10 17:00:27 +0200

    Refactors

--- build/dist/jest-config/assetsTransformer.js
+++
@@ -1,7 +0,0 @@
-var path = require("path");
-module.exports = {
-    process: function (src, filename, config, options) {
-        return 'module.exports = ' + JSON.stringify(path.basename(filename)) + ';';
-    },
-};
-//# sourceMappingURL=assetsTransformer.js.map
